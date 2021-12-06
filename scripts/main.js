7922ce01f69f468eeb18238e2fc0819eee26ddd9
Drew Smith <smithdrew555@yahoo.com>
2021-12-06 18:49:05 -0500

    removed uneeded js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-function resizeYoutubeVideos(){ //sets youtube videos to 16:9
-    let iframes = document.querySelectorAll(".youtube-video");
-    iframes.forEach(element => {
-        element.style.height = element.offsetWidth * 9. / 16. + "px";
-    });
-}
-
-resizeYoutubeVideos(); //sets youtube videos to 16:9 on load
-window.onresize = resizeYoutubeVideos; //sets youtube videos to 16:9 when resizing
-
 
 
 let select = document.querySelector(".filter"); //filter selection
@@ -42,21 +32,3 @@ if (select !== null){
     select.onchange = filter; //filter every time the filter changes
 }
 
-
-function adjustFooter() { //sets footer to bottom
-    let main = document.querySelector(".main")
-    let footer = document.querySelector("footer");
-    if(main.getBoundingClientRect().bottom + footer.offsetHeight + main.style.paddingBottom < window.innerHeight){
-        //if the footer is not at the bottom move it
-        footer.style.position = "fixed";
-        footer.style.bottom = 0;
-    }
-    else{
-        //reset footer
-        footer.style.position = null;
-        footer.style.bottom = null;
-    }
-}
-
-window.onload = adjustFooter;
-window.onchange = adjustFooter;
