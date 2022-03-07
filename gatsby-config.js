3efc56a90297b70a9b42efafeede91886eb253e6
Drew Smith <smithdrew555@yahoo.com>
2022-03-06 23:19:07 -0500

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,32 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
-    title: ``,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "Drew-Smiths-site",
+    siteUrl: "https://drew-j-smith.github.io/Drew-Smiths-site/",
   },
-  plugins: ["gatsby-plugin-sass", {
-    resolve: `gatsby-plugin-mdx`,
-    options: {
-      defaultLayouts: {
-        posts: require.resolve("./src/components/posts-layout.tsx"),
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          posts: require.resolve("./src/components/posts-layout.tsx"),
+        },
       },
     },
-  }, {
-      resolve: 'gatsby-source-filesystem',
+    {
+      resolve: "gatsby-source-filesystem",
       options: {
-        "name": "posts",
-        "path": "./src/posts/"
+        name: "posts",
+        path: "./src/posts/",
       },
-    }, {
+    },
+    {
       resolve: "gatsby-plugin-page-creator",
       options: {
         path: "./src/posts",
       },
-    },]
+    },
+  ],
+  pathPrefix: "/Drew-Smiths-site",
 };
