47318a0cdedc1d1e21985d4c9a8a6c11d0832c61
Drew Smith <smithdrew555@gmail.com>
2022-03-19 16:47:07 -0400

    added title to post-layout and add first post

--- src/components/posts-layout.tsx
+++ src/components/posts-layout.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import GlobalLayout from "./global-layout";
+import { PageProps } from "gatsby";
 
-type PostLayoutProps = {
-  children: JSX.Element;
+type PostLayoutProps = {};
+
+const post_layout = (props: PageProps<PostLayoutProps>) => {
+  return (
+    <GlobalLayout>
+      <>
+        <h1>{props.pageContext.frontmatter.title}</h1>
+        {props.children}
+      </>
+    </GlobalLayout>
+  );
 };
-
-const post_layout = (props: PostLayoutProps) => (
-  <GlobalLayout>{props.children}</GlobalLayout>
-);
 
 export default post_layout;
